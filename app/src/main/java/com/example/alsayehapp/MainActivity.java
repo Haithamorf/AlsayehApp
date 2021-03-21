f928f8c3e39ec1de3234743e880975319cd01d82
Haitham <haitham-01@hotmail.com>
2021-03-21 04:59:24 +0300

    Log in page. choosing account type users page. sign up page for tourist page.

--- app/src/main/java/com/example/alsayehapp/MainActivity.java
+++ app/src/main/java/com/example/alsayehapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.alsayehapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mLogin,mSignup,mForget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLogin= (Button) findViewById(R.id.LogInBtn);
+        mSignup= (Button) findViewById(R.id.signupBtn);
+        mForget= (Button) findViewById(R.id.forgetPassBtn);
+
+        mSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,userType.class);
+                startActivity(intent);
+                finish();
+                return;
+
+            }
+        });
     }
+
+
 }
