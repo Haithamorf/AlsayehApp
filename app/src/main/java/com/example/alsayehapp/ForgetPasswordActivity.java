59eebcc87609251d8b5106f1c56de5143f4b4298
Mohammed Alosaimi <mohammedalosaimi@gmail.com>
2021-03-24 15:07:05 +0300

    add back to main activity button to forgetPassword page

--- app/src/main/java/com/example/alsayehapp/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/alsayehapp/ForgetPasswordActivity.java
@@ -2,19 +2,35 @@ package com.example.alsayehapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ToggleButton;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
+    private Button buSend,buBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+        buSend = (Button) findViewById(R.id.sendForget);
+        buBack = (Button) findViewById(R.id.goMainPage);
+
+        buBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgetPasswordActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+
+            }
+        });
     }
 
     public void changeMethod(View view) {
