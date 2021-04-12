e545124ca3d7a44b11d0883a01638e75be8fe5be
Mohammed Alosaimi <mohammedalosaimi@gmail.com>
2021-04-12 14:51:39 +0300

    and tour guide home page

---
+++ app/src/main/java/com/example/alsayehapp/tourGuideHomePage.java
@@ -0,0 +1,14 @@
+package com.example.alsayehapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tourGuideHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tour_guide_home_page);
+    }
+}
